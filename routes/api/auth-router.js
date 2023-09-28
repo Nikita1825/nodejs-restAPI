609113e0348557de9b20a10b7d236793928e5876
Nikita1825 <nikitapostnov1825@gmail.com>
2023-09-29 02:50:29 +0300

    hw5

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -3,7 +3,7 @@ const authRouter = express.Router();
 import { isValidid } from "../../middlewares/index.js";
 import Joi from "joi";
 import authController from "../../controllers/auth-controller.js";
-import { authenticate } from "../../middlewares/index.js";
+import { authenticate, upload, resizeAvatar } from "../../middlewares/index.js";
 
 const userSignUpSchema = Joi.object({
   password: Joi.string().min(6).required(),
@@ -20,5 +20,7 @@ authRouter.post("/signin", userSignInSchema, isValidid);
 
 authRouter.get("/current", authenticate, authController.getCurrent)
 authRouter.post("/signout", authenticate, authController.signout);
+authRouter.patch("/avatars", authenticate, upload.single("avatars"), resizeAvatar, authController.updateAvatar);
+
 
 export default authRouter;
